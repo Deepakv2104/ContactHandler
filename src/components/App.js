ab522afd22b153b4c323f4372afe4bb100f253ba
Deepak Vishwakarma <20eg106248@anurag.edu.in>
2023-02-28 21:28:56 +0530

    added functionality to the components and storing in local device

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,25 @@
-
+import React,{useState, useEffect} from 'react';
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-    id: "1",
-    name:"Deepak",
-    email:"deepv2104@gmail.com",
-    },
-    {
-    id: "2",
-    name:"Rohan",
-    email:"Rohan@gmail.com"
-    },
-  ];
+  const LOCAL_STORAGE_KEY = "contacts";
+ const [contacts, setContacts] = useState([]);
+ const addContactHandler = (contact) =>{
+  console.log(contact);
+  setContacts([...contacts,contact]);
+ };
+
+ useEffect(()=>{
+  localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
+ },[contacts]);
   return (
     
     <div className="ui container">
       <Header/>
-      <AddContact/>
+      <AddContact addContactHandler ={addContactHandler}/>
       <ContactList contacts={contacts}/>
     </div>
   );
